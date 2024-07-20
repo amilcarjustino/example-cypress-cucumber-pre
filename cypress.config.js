b1b1caa9510f7163c75c117fec0e1184eaf4b629
Amílcar Justino <amilcarjustino@gmail.com>
2024-07-20 13:02:54 +0100

    first commit

---
+++ cypress.config.js
@@ -0,0 +1,25 @@
+const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const {addCucumberPreprocessorPlugin} = require("@badeball/cypress-cucumber-preprocessor");
+const {createEsbuildPlugin} = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+
+async function setupNodeEvents(on, config) {
+    await addCucumberPreprocessorPlugin(on, config);
+
+    on(
+        "file:preprocessor",
+        createBundler({
+            plugins: [createEsbuildPlugin(config)],
+        })
+    );
+
+    return config;
+}
+
+module.exports = defineConfig({
+    e2e: {
+        baseUrl: "https://duckduckgo.com",
+        specPattern: "**/*.feature",
+        setupNodeEvents,
+    },
+});
